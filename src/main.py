1835bf93c9760f433ad5b740f676f77035ed9d72
DESKTOP-VGH69S8\AlexeyShakov <alexei_96@inbox.ru>
2025-01-25 14:48:46 +0300

    not needed files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,3 @@
 from fastapi import FastAPI
 
-from movies import MOVIE_ROUTES
-from actors import ACTOR_ROUTES
-
 app = FastAPI()
-
-app.include_router(MOVIE_ROUTES)
-app.include_router(ACTOR_ROUTES)
